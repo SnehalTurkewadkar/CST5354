5c113998f086b49c39d671119907c190b1f38741
Snehal Turkewadkar <snehal.turkewadkar26@gmail.com>
2023-06-23 00:14:27 +0530

    second commit

--- src/test/java/day22June2023/Test1.java
+++ src/test/java/day22June2023/Test1.java
@@ -4,6 +4,9 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Hi first class first line");
 		int i=10;
+		float a=12.23f;
+		for(a=0.1f;a<=0.9;a+=0.1)
+			System.out.println(a);
 	}
 
 }
